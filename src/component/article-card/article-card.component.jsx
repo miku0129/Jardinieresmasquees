a75790331ec1fa6c6abe63f56dcc088da0a73f6c
miku0129 <svartkatt39@gmail.com>
2023-08-31 20:47:04 +0200

    implemented getting posts from fb

--- src/component/article-card/article-card.component.jsx
+++ src/component/article-card/article-card.component.jsx
@@ -1,31 +1,29 @@
 import "./article-card.component.scss";
 
 const ArticleCard = ({ article }) => {
-  const { article_summary, article_image_url, article_original_resource } =
-    article;
+  // const src = article_image_url
+  //   ? article_image_url
+  //   : "https://i.ibb.co/kGqNLh5/Screenshot-2023-08-20-143312.jpg";
 
-  const src = article_image_url
-    ? article_image_url
-    : "https://i.ibb.co/kGqNLh5/Screenshot-2023-08-20-143312.jpg";
-
-  const text =
-    article_summary.length < 70
-      ? article_summary
-      : article_summary.slice(0, 70) + "...";
-
+  const { created_time, message } = article;
+  const text = message.length < 80 ? message : message.slice(0, 80) + "...";
+  console.log("message", message);
   return (
     <div>
-      <a
+      {/* <a
         href={article_original_resource}
-        class="blog-card-link"
+        className="blog-card-link"
         target="_blank"
         rel="noreferrer"
-      >
-        <div class="blog-card">
-          <img src={src} alt={article_summary} />
-          <p class="blog-description">{text}</p>
-        </div>
-      </a>
+      > */}
+      <div className="blog-card">
+        <img
+          src="https://i.ibb.co/kGqNLh5/Screenshot-2023-08-20-143312.jpg"
+          alt="image"
+        />
+        <p className="blog-description">{text}</p>
+      </div>
+      {/* </a> */}
     </div>
   );
 };
