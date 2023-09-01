55a75df3c27d97589ce4803d5aa5cc96b4b32f95
miku0129 <svartkatt39@gmail.com>
2023-09-01 14:12:13 +0200

    add date to article-card

--- src/component/article-card/article-card.component.jsx
+++ src/component/article-card/article-card.component.jsx
@@ -1,12 +1,16 @@
 import "./article-card.component.scss";
 
 const ArticleCard = ({ article }) => {
-  
   const { created_time, message, permalink_url, full_picture } = article;
+
   const text = message.length < 80 ? message : message.slice(0, 80) + "...";
+
   const src = full_picture
     ? full_picture
     : "https://i.ibb.co/kGqNLh5/Screenshot-2023-08-20-143312.jpg";
+
+  const published_date = created_time.slice(0, 10);
+
   return (
     <div>
       <a
@@ -15,13 +19,11 @@ const ArticleCard = ({ article }) => {
         target="_blank"
         rel="noreferrer"
       >
-      <div className="blog-card">
-        <img
-          src={src}
-          alt={created_time}
-        />
-        <p className="blog-description">{text}</p>
-      </div>
+        <div className="blog-card">
+          <img src={src} alt={created_time} />
+          <p>{published_date} | Actualités</p>
+          <p className="blog-description">{text}</p>
+        </div>
       </a>
     </div>
   );
