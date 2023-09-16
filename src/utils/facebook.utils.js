ce62fe155b5a482fd11cb2bc713203a4179a33d1
miku0129 <svartkatt39@gmail.com>
2023-09-16 20:45:59 +0200

    add event data from facebook

--- src/utils/facebook.utils.js
+++ src/utils/facebook.utils.js
@@ -20,14 +20,13 @@ export const getPostsFromFacebook = async () => {
     const res = await axios.get(
       `https://graph.facebook.com/v17.0/${process.env.REACT_APP_PAGE_ID}/feed?fields=id,permalink_url,created_time,message,full_picture,story&access_token=${process.env.REACT_APP_FB_ACCESS_TOKEN}`
     );
-    console.log(res.data.data);
     return res.data.data;
   } catch (e) {
     console.log(e);
   }
 };
 
-export const getEventsFromAllPosts = async () => {
+export const getEventsFromPosts = async () => {
   const allPosts = await getPostsFromFacebook();
   const eventPosts = allPosts.filter((post) => {
     return (
@@ -43,13 +42,15 @@ export const getEventsFromAllPosts = async () => {
 
   const responses = event_ids.map((event_id) =>
     axios.get(
-      `https://graph.facebook.com/v17.0/${event_id}?access_token=${process.env.REACT_APP_FB_ACCESS_TOKEN}`
+      `https://graph.facebook.com/v17.0/${event_id}?fields=id,name,cover,place,description&access_token=${process.env.REACT_APP_FB_ACCESS_TOKEN}`
     )
   );
 
   const response = await Promise.all([...responses]);
 
-  const result = response.map((res) => res.data);
+  const result = response.map((res, idx) => {
+    const obj = { ...res.data, permalink_url: eventPosts[idx].permalink_url };
+    return obj;
+  });
   return result;
 };
-
