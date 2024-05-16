14294805e5980510e3a4e6a844da97535421476c
miku0129 <svartkatt39@gmail.com>
2024-05-16 22:53:49 +0200

    feat: add FlickerAPI

---
+++ src/component/gallery/galeries.component.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { createFlickr } from "flickr-sdk";
+import ImageGallery from "react-image-gallery";
+
+const Gallery = () => {
+  const [photos, setPhotos] = useState([]);
+  // const [images, setImages] = useState([]);
+
+  const { flickr } = createFlickr(process.env.REACT_APP_FLICKR_API_KEY);
+
+  useEffect(() => {
+    const serverId = process.env.REACT_APP_FLICKR_SERVER_ID;
+
+    const getPhotos = async () => {
+      const res = await flickr("flickr.photosets.getPhotos", {
+        photoset_id: process.env.REACT_APP_FLICKR_PHOTOSET_ID,
+        user_id: process.env.REACT_APP_FLICKR_USER_ID,
+      });
+      const photos = res.photoset.photo.map((item) => {
+        const photoUrl = `https://live.staticflickr.com/${serverId}/${item.id}_${item.secret}.jpg`;
+        const obj = {
+          original: photoUrl,
+          thumbnail: photoUrl,
+          originalHeight: 500,
+          thumbnailHeight: 70,
+        };
+        return obj;
+      });
+
+      setPhotos(photos);
+    };
+    getPhotos();
+  }, []);
+  console.log("photos", photos);
+
+  return (
+    <div className="image-gallery-container">
+      <ImageGallery
+        items={photos}
+        showNav={true}
+        autoPlay={false}
+        showFullscreenButton={false}
+        useBrowserFullscreen={false}
+        showPlayButton={false}
+        showBullets={true}
+      />
+    </div>
+  );
+};
+
+export default Gallery;
--- src/component/main/main.component.jsx
+++ src/component/main/main.component.jsx
@@ -1,5 +1,4 @@
 import { Fragment } from "react";
-import ImageGallery from "react-image-gallery";
 import { AnchorSection } from "react-anchor-navigation";
 
 import ArticlesPreview from "../../route/articles-preview/articles-preview.component";
@@ -7,52 +6,23 @@ import ContentHeader from "../content-header/content-header.compoment";
 import ContentIntroduction from "../content-introduction/content-introduction.component";
 import ContentContact from "../content-contact/content-contact.component";
 
-import { ACTIVITY_PHOTO_IMAGES } from "../../utils/data.utils/activity-photo-data";
-
 import "react-image-gallery/styles/css/image-gallery.css";
 import "./main.styles.scss";
 
-const Main = () => {
-  const originalImages = ACTIVITY_PHOTO_IMAGES.activity_photo;
-  const images = originalImages.map((item) => {
-    const obj = {
-      original: item.image_url,
-      thumbnail: item.image_url,
-      originalHeight: 500,
-      thumbnailHeight: 70,
-    };
-    return obj;
-  });
+import Gallery from "../gallery/galeries.component";
 
+const Main = () => {
   return (
     <Fragment>
       <div className="main-container">
-        {/* <div> */}
         <AnchorSection id="accueil" />
         <ContentHeader />
-        {/* </div> */}
-
         <div className="contents-container">
           <AnchorSection id="propos" />
           <ContentIntroduction />
-
-          <div className="image-gallery-container">
-            <ImageGallery
-              items={images}
-              showNav={true}
-              autoPlay={false}
-              showFullscreenButton={false}
-              useBrowserFullscreen={false}
-              showPlayButton={false}
-              showBullets={true}
-            />
-          </div>
-
+          <Gallery />
           <AnchorSection id="infos" />
-          {/* <div> */}
           <ArticlesPreview />
-          {/* </div> */}
-
           <AnchorSection id="contact" />
           <ContentContact />
         </div>
