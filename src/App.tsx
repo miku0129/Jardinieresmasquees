8141ab16e65e8a04ef2dc9e84c28868d90bda3e3
miku0129 <svartkatt39@gmail.com>
2024-05-26 21:11:19 +0200

    feat: add route for /gallery

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AnchorProvider } from "react-anchor-navigation";
 
 import BasicLayout from "./route/basic-layout/basic-layout";
 import Home from "./route/home/home.route";
+import Gallery from "./component/gallery/gallery.component";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<BasicLayout />}>
           <Route index element={<Home />} />
+          <Route path="gallery" element={<Gallery />} />
         </Route>
       </Routes>
     </AnchorProvider>
