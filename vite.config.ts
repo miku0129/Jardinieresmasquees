218413ec79a730cecfd47bbfe78986f3b1ec3e33
miku0129 <svartkatt39@gmail.com>
2026-03-01 15:44:25 +0100

    pref: add tailwindcss with Prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import tailWindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
@@ -10,7 +11,7 @@ export default defineConfig({
   build: {
     outDir: "build", // ビルドの出力先ディレクトリ
   },
-  plugins: [react(), svgr()],
+  plugins: [react(), tailWindcss(), svgr()],
   define: {
     global: {},
   },
