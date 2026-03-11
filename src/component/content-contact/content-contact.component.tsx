f497495d18c3c1e881db08f3cdc5523d41751a30
miku0129 <svartkatt39@gmail.com>
2026-03-11 14:48:48 +0100

    refactor: simplefy the style in ContentContact

--- src/component/content-contact/content-contact.component.tsx
+++ src/component/content-contact/content-contact.component.tsx
@@ -1,29 +1,27 @@
 import FacebookLogo from "../../assets/facebook-svgrepo-com.svg?react";
-import { Logo, ContactContainer } from "./content-contact.styles";
+import { Logo } from "./content-contact.styles";
 
-const ContentContact = () => {
+export default function ContentContact() {
   return (
-    <ContactContainer>
-      <div className="icons">
-        <div className="group-logo">
-          <Logo></Logo>
-        </div>
-        <div className="facebook-logo">
-          <a
-            href="https://www.facebook.com/jardinieresmasquees"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FacebookLogo style={{ width: "30px", height: "30px" }} />
-          </a>
-        </div>
+    <div className="px-content-side mb-5 flex flex-col justify-center gap-5">
+      <section className="flex flex-col place-self-center-safe">
+        <p className="md:text-xl">
+          Si vous êtes intéressé, veuillez nous contacter à cette adresse.
+        </p>
+        <p className="text-gray-500 md:text-xl">
+          jardinieres.masquees@protonmail.com
+        </p>
+      </section>
+      <div className="flex items-end gap-1 place-self-center-safe">
+        <Logo />
+        <a
+          href="https://www.facebook.com/jardinieresmasquees"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FacebookLogo style={{ width: "30px", height: "30px" }} />
+        </a>
       </div>
-      <div className="contact-sub-container">
-        <p>Si vous êtes intéressé, veuillez nous contacter à cette adresse.</p>
-        <p>jardinieres.masquees@protonmail.com</p>
-      </div>
-    </ContactContainer>
+    </div>
   );
-};
-
-export default ContentContact;
+}
