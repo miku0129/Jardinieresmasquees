90bcc0c0fb9c2beaf54a9857163f09223b35f46b
miku0129 <svartkatt39@gmail.com>
2026-03-11 10:03:08 +0100

    refactor: remove styled-components from BasicLayout

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import { Route, Routes } from "react-router-dom";
 
 import Gallery from "./component/gallery.component";
 import PhotoViewer from "./component/photo-viewer.component";
-import BasicLayout from "./route/basic-layout/basic-layout";
+import BasicLayout from "./route/basic-layout";
 import Home from "./route/home/home.route";
 
-function App() {
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<BasicLayout />}>
         <Route index element={<Home />} />
-        <Route path="gallery" element={<Gallery />}/>
-        <Route path="photoviewer/:photoIndex" element={<PhotoViewer/>}/>
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="photoviewer/:photoIndex" element={<PhotoViewer />} />
       </Route>
     </Routes>
   );
 }
-
-export default App;
--- src/route/basic-layout/basic-layout.styles.ts
+++
@@ -1,8 +0,0 @@
-import { styled } from "styled-components";
-
-export const BasicLayoutContainer = styled.div`
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-  min-height: 100vh;
-  position: relative;
-`;
